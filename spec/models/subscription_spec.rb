123fa4bf8bd0b5dc0211345d93285c092414998a
Dylan Perry <dylanbperry@gmail.com>
2024-03-12 20:30:03 -0700

    Add model spec tests for tea, customer, and subscription

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Subscription, type: :model do
+    describe 'validations' do
+        it { should validate_presence_of(:title) }
+        it { should validate_presence_of(:price) }
+        it { should validate_presence_of(:frequency) }
+        it { should validate_presence_of(:status) }
+
+        it { should belong_to(:customer) }
+        it { should belong_to(:tea) }
+    end
+end
