22cd6c6f4831eaa0e0305ac6ebce7287ff52b713
Dylan Perry <dylanbperry@gmail.com>
2024-03-14 15:04:20 -0700

    Remove unused status activation code

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,4 @@
 class Subscription < ApplicationRecord
-    # after_create :activate_subscription
-
     validates :title, :price, :frequency, :status, presence: :true
 
     enum status: { cancelled: 0, active: 1 }
@@ -8,8 +6,4 @@ class Subscription < ApplicationRecord
     belongs_to :customer
     has_many :tea_subscriptions
     has_many :teas, through: :tea_subscriptions
-
-    def activate_subscription
-        self.status = 1
-    end
 end
