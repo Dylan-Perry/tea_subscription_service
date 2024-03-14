f5075e6992c842ddbf1c6e610a65852b8879c371
Dylan Perry <dylanbperry@gmail.com>
2024-03-14 13:44:14 -0700

    Add sad path testing for subscription create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_response 
+
+    def not_found_response
+        render json: { error: 'Sorry, your credentials are bad!' }, status: :unauthorized
+    end
+
     def find_customer_by_email(email)
         Customer.find_by(email: email)
     end
--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Subscriptions Request API' do
     describe "Subscription Create" do
-        describe "happy path" do
-            before :each do
-                @customer1 = Customer.create!(
-                    first_name: "General",
-                    last_name: "Iroh",
-                    email: "dragonofthewest@gmail.com",
-                    address: "123 Pi Cho Ct, Ba Sing Se"
-                    )
-
-                @tea1 = Tea.create!(
-                    title: "Ginseng",
-                    description: "Ginseng has been used for improving overall health. It has also been used to strengthen the immune system and help fight off stress and disease.",
-                    temperature: "208°F",
-                    brew_time: "5 - 10 minutes"
+        before :each do
+            @customer1 = Customer.create!(
+                first_name: "General",
+                last_name: "Iroh",
+                email: "dragonofthewest@gmail.com",
+                address: "123 Pi Cho Ct, Ba Sing Se"
                 )
-            end
 
+            @tea1 = Tea.create!(
+                title: "Ginseng",
+                description: "Ginseng has been used for improving overall health. It has also been used to strengthen the immune system and help fight off stress and disease.",
+                temperature: "208°F",
+                brew_time: "5 - 10 minutes"
+            )
+        end
+
+        describe "happy path" do
             it "creates a subscription for a customer and tea" do
                 params = {
                     customer_email: "#{@customer1.email}",
@@ -34,8 +34,6 @@ RSpec.describe 'Subscriptions Request API' do
                 post api_v1_customer_subscriptions_path(@customer1), headers: headers, params: JSON.generate(params)
 
                 result = JSON.parse(response.body, symbolize_names: true)
-
-                require 'pry'; binding.pry
 
                 # Test response and status code
                 expect(response).to be_successful
@@ -61,6 +59,63 @@ RSpec.describe 'Subscriptions Request API' do
                 expect(result[:data][:attributes][:price]).to eq(6.00)
                 expect(result[:data][:attributes][:frequency]).to eq("1 week")
                 expect(result[:data][:attributes][:status]).to eq("active")
+            end
+        end
+
+        describe "sad path" do
+            it "errors out if required params are not provided" do
+                params = {
+                    customer_email: "#{@customer1.email}",
+                    frequency: "1 week"
+                }
+                headers = { 
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+                
+                post api_v1_customer_subscriptions_path(@customer1), headers: headers, params: JSON.generate(params)
+
+                result = JSON.parse(response.body, symbolize_names: true)
+
+                # Test response and status code
+                expect(response).to_not be_successful
+                expect(response.status).to eq(401)
+
+                # JSON formatting according to front end spec
+                expect(result).to be_a(Hash)
+
+                # Test missing attribute errors
+                expect(result).to have_key(:title)
+                expect(result).to have_key(:price)
+
+                expect(result[:title]).to include("can't be blank")
+                expect(result[:price]).to include("can't be blank")
+            end
+
+            it "errors out if provided email does not match a user" do
+                params = {
+                    customer_email: "borgus@borgus.com",
+                    title: "#{@tea1.title}",
+                    price: 6.00,
+                    frequency: "1 week"
+                }
+                headers = { 
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+                
+                post api_v1_customer_subscriptions_path(@customer1), headers: headers, params: JSON.generate(params)
+
+                result = JSON.parse(response.body, symbolize_names: true)
+
+                # Test response and status code
+                expect(response).to_not be_successful
+                expect(response.status).to eq(401)
+
+                # JSON formatting according to front end spec
+                expect(result).to be_a(Hash)
+
+                expect(result[:error]).to eq("Sorry, your credentials are bad!")
             end
         end
     end
